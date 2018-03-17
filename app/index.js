91eac240d59c17ef21eed00d3ba2847f6dca7fe9
Ewerton Oliveira <ewerton.tccmbr@gmail.com>
2018-03-17 11:16:25 -0300

    TypedJs funcionando usando NPM.

---
+++ app/index.js
@@ -0,0 +1,8 @@
+var options = {
+  strings: ["Hello world!", "TypedJs Funcionando com NPM!"],
+  typeSpeed: 100,
+	backDelay: 200,
+	loop: true
+}
+
+var typed = new Typed(".type", options);
--- js/default.js
+++
@@ -1,6 +0,0 @@
-$(".type").typed({
-	strings: ["Hello world!", "TypeJs Funcionando Jonathan!"],
-	typeSpeed: 100,
-  backDelay: 200,
-  loop: true
-});
